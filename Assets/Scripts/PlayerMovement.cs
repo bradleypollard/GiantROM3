9ea530b57a558e8687b1c72e6551149a784f1715
bradleypollard <bp912@ic.ac.uk>
2016-08-08 22:26:46 +0100

    Normalise movement direction so speed is constant, adjust playermovement.cs to support multiple players, remove movement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,16 +10,17 @@ public class PlayerMovement : MonoBehaviour
 
 	[Header("Settings")]
 	[Range(0.18f, 0.05f)][SerializeField] float speed;
+  [Range(1, 4)][SerializeField] int playerIndex;
 
-	void
+  void
 	Update()
 	{
-		Vector3 moveDirection = movementOrientation.TransformDirection (new Vector3 (Input.GetAxisRaw ("Horizontal_P1"), 0, Input.GetAxisRaw ("Vertical_P1")));
+		Vector3 moveDirection = movementOrientation.TransformDirection (new Vector3 (Input.GetAxisRaw ("Horizontal_P" + playerIndex.ToString()), 0, Input.GetAxisRaw ("Vertical_P" + playerIndex.ToString())));
 	
 		if (moveDirection != Vector3.zero)
 		{
 			moveDirection = new Vector3 (moveDirection.x, 0, moveDirection.z);
-			transform.Translate(moveDirection * speed);
+			transform.Translate(moveDirection.normalized * speed);
 
 			Vector3 rotationDirection = moveDirection;
 			//rotationDirection = new Vector3 (rotationDirection.x, -0.2f, rotationDirection.z);
