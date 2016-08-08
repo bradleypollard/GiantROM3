f506f9c40e175afa8fc114153112096083188ffb
UaSirideain <man.dude.2@gmail.com>
2016-08-08 19:14:21 +0100

    Resolved some missing assets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,16 +14,16 @@ public class PlayerMovement : MonoBehaviour
 	void
 	Update()
 	{
-//		Vector3 moveDirection = movementOrientation.TransformDirection (new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")));
-//	
-//		if (moveDirection != Vector3.zero)
-//		{
-//			moveDirection = new Vector3 (moveDirection.x, 0, moveDirection.z);
-//			transform.Translate (moveDirection * speed);
-//
-//			Vector3 rotationDirection = moveDirection;
-//			//rotationDirection = new Vector3 (rotationDirection.x, -0.2f, rotationDirection.z);
-//			characterMesh.rotation = Quaternion.LookRotation (rotationDirection);
-//		}
+		Vector3 moveDirection = movementOrientation.TransformDirection (new Vector3 (Input.GetAxisRaw ("Horizontal_P1"), 0, Input.GetAxisRaw ("Vertical_P1")));
+	
+		if (moveDirection != Vector3.zero)
+		{
+			moveDirection = new Vector3 (moveDirection.x, 0, moveDirection.z);
+			transform.Translate(moveDirection * speed);
+
+			Vector3 rotationDirection = moveDirection;
+			//rotationDirection = new Vector3 (rotationDirection.x, -0.2f, rotationDirection.z);
+			characterMesh.rotation = Quaternion.LookRotation (rotationDirection);
+		}
 	}
 }
