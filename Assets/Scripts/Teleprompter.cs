d479d9d70186007f6bc9070e7955bd2cd1398ec5
bradleypollard <bp912@ic.ac.uk>
2016-08-08 23:42:48 +0100

    Teleprompter now has two queues, one for generated promts and one for displayed prompts to the user. Buttons moved between them on an exponential timer

--- Assets/Scripts/Teleprompter.cs
+++ Assets/Scripts/Teleprompter.cs
@@ -1,36 +1,100 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class Teleprompter : MonoBehaviour {
+public class Teleprompter : MonoBehaviour
+{
 
-  public int speakerID = -1;
+  [Header("Settings")]
+  [Range(0, 4)]
+  [SerializeField]
+  int speakerID = 0;
+  [Range(50, 200)]
+  [SerializeField]
+  int maxQueueSize = 100;
 
   private string[] buttons = { "A", "B", "X", "Y" };
-  private Queue<string> prompts;
-  private string label = "";
+  private Queue<string> visiblePrompts;
+  private Queue<string> upcomingPrompts;
+  private string telepromter = "";
+  private GUIStyle hitStyle = new GUIStyle();
+  private string hit = "";
+  private GUIStyle missStyle = new GUIStyle();
+  private string miss = "";
+  private float timeTillNextPrompt = 0f;
 
-	// Use this for initialization
-	void Start () {
-    prompts = new Queue<string>();
-    for (int i = 0; i < 100; ++i)
+  // Use this for initialization
+  void Start()
+  {
+    visiblePrompts = new Queue<string>();
+    upcomingPrompts = new Queue<string>();
+    hitStyle.normal.textColor = Color.green;
+    missStyle.normal.textColor = Color.red;
+
+    SetSpeakerID(1);
+  }
+
+  // Called when a new speaker arrives on the stage
+  public void SetSpeakerID(int _id)
+  {
+    speakerID = _id;
+    AddMorePrompts();
+  }
+
+  // Called once the current speaker has finished
+  private void ClearSpeakerID()
+  {
+    speakerID = 0;
+  }
+
+  // Called when the handle is cranked, or a new speaker arrives
+  public void AddMorePrompts()
+  {
+    for (int i = upcomingPrompts.Count - 1; i < maxQueueSize; ++i)
     {
       string button = buttons[Random.Range(0, buttons.Length)];
-      prompts.Enqueue(button);
-      label += button;
+      upcomingPrompts.Enqueue(button);
     }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	  if (prompts.Count > 0 && Input.GetButtonDown(prompts.Peek() + "_P" + speakerID))
-    {
-      prompts.Dequeue();
-      label = label.Substring(1);
-    }
-	}
+  }
 
+  // Update is called once per frame
+  void Update()
+  {
+    // Reveal new button to player
+    if (upcomingPrompts.Count > 0 && timeTillNextPrompt <= 0f)
+    {
+      string button = upcomingPrompts.Dequeue();
+      visiblePrompts.Enqueue(button);
+      telepromter += button;
+      timeTillNextPrompt = 2 * Mathf.Exp(-5 * upcomingPrompts.Count / maxQueueSize);
+    }
+
+    timeTillNextPrompt -= Time.deltaTime;
+
+    // Check if player hit a prompt
+    foreach (string b in buttons)
+    {
+      if (visiblePrompts.Count > 0 && Input.GetButtonDown(b + "_P" + speakerID))
+      {
+        string button = visiblePrompts.Dequeue();
+        telepromter = telepromter.Substring(1);
+        if (button == b)
+        {
+          hit += button;
+        }
+        else
+        {
+          miss += button;
+        }
+      }
+    }
+
+  }
+
+  // Debug drawing the input string
   void OnGUI()
   {
-    GUI.Label(new Rect(0, 0, 500, 20), label);
+    GUI.Label(new Rect(0, 0, 500, 20), telepromter);
+    GUI.Label(new Rect(0, 30, 500, 20), hit, hitStyle);
+    GUI.Label(new Rect(0, 60, 500, 20), miss, missStyle);
   }
 }
