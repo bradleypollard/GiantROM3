70db7cfb4c45758a6e2e77025b48104a6d89944d
Adam Marsh <adam.marsh@rocketmail.com>
2016-08-08 22:51:38 +0100

    Created Player Prefab as useful for testing.
    Basic ability to pick an object up.

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interaction : MonoBehaviour
+{
+
+	public Transform characterMesh;
+
+	public GameObject objectInHands;
+
+	void FixedUpdate ()
+	{
+		if (objectInHands) {
+			if (Input.GetKeyDown (KeyCode.LeftAlt)) {
+				print ("Dropping " + objectInHands.transform.name);
+				objectInHands.transform.parent = null;
+				objectInHands = null;
+			}
+		} else {
+
+			Vector3 fwd = characterMesh.TransformDirection (Vector3.forward);
+			Debug.DrawRay (characterMesh.position, fwd * 1, Color.green);
+
+			RaycastHit hit;
+
+			if (Physics.Raycast (characterMesh.position, fwd, out hit, 1)) {
+				if (hit.transform.tag == "Interactable") {
+					print ("You can interact " + hit.transform.name);
+					if (Input.GetKeyDown (KeyCode.Space)) {
+						objectInHands = hit.transform.gameObject;
+						objectInHands.transform.parent = this.transform;
+						print ("Picking up object");
+					}
+				} else {
+					print ("You can't interact " + hit.transform.name);
+				}
+			}
+
+		}
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent (typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-	[Header("References")]
+	[Header ("References")]
 	[SerializeField] Transform movementOrientation;
 	[SerializeField] Transform characterMesh;
 
-	[Header("Settings")]
-	[Range(0.18f, 0.05f)][SerializeField] float speed;
+	[Header ("Settings")]
+	[Range (0.18f, 0.05f)][SerializeField] float speed;
 
 	void
-	Update()
+	Update ()
 	{
 		Vector3 moveDirection = movementOrientation.TransformDirection (new Vector3 (Input.GetAxisRaw ("Horizontal_P1"), 0, Input.GetAxisRaw ("Vertical_P1")));
 	
-		if (moveDirection != Vector3.zero)
-		{
+		if (moveDirection != Vector3.zero) {
 			moveDirection = new Vector3 (moveDirection.x, 0, moveDirection.z);
-			transform.Translate(moveDirection * speed);
+			transform.Translate (moveDirection * speed);
 
 			Vector3 rotationDirection = moveDirection;
 			//rotationDirection = new Vector3 (rotationDirection.x, -0.2f, rotationDirection.z);
