3be2c4c926d460f3ff501b9771c06dd956b55dcc
bradleypollard <bp912@ic.ac.uk>
2016-08-08 18:00:27 +0100

    Incredibly basic teleprompter

---
+++ Assets/Scripts/Teleprompter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Teleprompter : MonoBehaviour {
+
+  public int speakerID = -1;
+
+  private string[] buttons = { "A", "B", "X", "Y" };
+  private Queue<string> prompts;
+  private string label = "";
+
+	// Use this for initialization
+	void Start () {
+    prompts = new Queue<string>();
+    for (int i = 0; i < 100; ++i)
+    {
+      string button = buttons[Random.Range(0, buttons.Length)];
+      prompts.Enqueue(button);
+      label += button;
+    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  if (prompts.Count > 0 && Input.GetButtonDown(prompts.Peek() + "_P" + speakerID))
+    {
+      prompts.Dequeue();
+      label = label.Substring(1);
+    }
+	}
+
+  void OnGUI()
+  {
+    GUI.Label(new Rect(0, 0, 500, 20), label);
+  }
+}
