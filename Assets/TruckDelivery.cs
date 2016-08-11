df5b222798d01e40bc1b60611498417afcab8c1c
Adam Marsh <adam.marsh@rocketmail.com>
2016-08-11 07:54:01 +0100

    Truck now has animation, will turn up, chuck a hard drive off and then leave.

--- Assets/TruckDelivery.cs
+++ Assets/TruckDelivery.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class TruckDelivery : MonoBehaviour
 {
@@ -7,8 +8,20 @@ public class TruckDelivery : MonoBehaviour
 	public GameObject hardDrivePrefab;
 	public string childNameForColour;
 	public Transform spawnPoint;
+	public Transform deliveryPointForTruck;
+	[Range (1, 4)]
+	public float truckAnimationTime;
 
 	public Vector3 forceToApply;
+
+	private Sequence truckDeliverySequence;
+
+	private Vector3 startPos;
+
+	void Start ()
+	{
+		startPos = transform.position;
+	}
 
 	void Update ()
 	{
@@ -18,6 +31,17 @@ public class TruckDelivery : MonoBehaviour
 	}
 
 	public void HardDriveDelivery ()
+	{
+		truckDeliverySequence = DOTween.Sequence ();
+
+
+		truckDeliverySequence.Append (transform.DOMove (deliveryPointForTruck.position, truckAnimationTime).OnComplete (ChuckHardDriveOutBack));
+		truckDeliverySequence.AppendInterval (0.5f);
+		truckDeliverySequence.Append (transform.DOMove (startPos, truckAnimationTime));
+		truckDeliverySequence.Play ();
+	}
+
+	public void ChuckHardDriveOutBack ()
 	{
 		GameObject newHD = Instantiate (hardDrivePrefab, spawnPoint.position, Quaternion.identity) as GameObject;
 		int randomNumber = Mathf.RoundToInt (Random.Range (1, 4));
