0e1c5a3f3fca90243bc8ecdbf31f43cf546bb2ed
bradleypollard <bp912@ic.ac.uk>
2016-08-12 01:15:08 +0100

    Reworked raycast for interaction. Players can now grab items 1.5m away, but the raycast checks for items 5m ahead. Interactive items should have a hitbox of 3x3x3m for best results. Player movement speed has been changed to 0.12

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -4,76 +4,99 @@ using System.Collections;
 public class Interaction : MonoBehaviour
 {
 
-	public Transform characterMesh;
+  public Transform characterMesh;
 
-	public GameObject objectInHands;
+  public GameObject objectInHands;
 
-	public PlayerMovement playerMovement;
+  public PlayerMovement playerMovement;
 
   public float rayLength = 5f;
   public float grabDistance = 1.5f;
 
-	void Start ()
-	{
-		playerMovement = gameObject.GetComponent<PlayerMovement> ();
-	}
+  void Start()
+  {
+    playerMovement = gameObject.GetComponent<PlayerMovement>();
+  }
 
-	void FixedUpdate ()
-	{
-		if (objectInHands) {
-			if (Input.GetButtonDown ("B_P" + playerMovement.playerIndex)) {
-				ReleaseObject ();
-			}
-		} else {
-
-			Vector3 fwd = characterMesh.TransformDirection (Vector3.forward);
+  void FixedUpdate()
+  {
+    if (objectInHands != null)
+    {
+      if (Input.GetButtonDown("B_P" + playerMovement.playerIndex))
+      {
+        ReleaseObject();
+      }
+    }
+    else if (Input.GetButtonDown("A_P" + playerMovement.playerIndex))
+    {
+      Vector3 fwd = characterMesh.TransformDirection(Vector3.forward);
       Ray ray = new Ray(characterMesh.position, fwd);
       Ray backRay = new Ray(characterMesh.position + rayLength * fwd, -fwd);
       Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.green);
-
-			RaycastHit hit;
-      bool foundItem = false;
+      
+      // Interact layer is layer 9
       int layer = 1 << 9;
-      if (!Physics.Raycast(ray, out hit, rayLength, layer))
-      {
-        foundItem = Physics.Raycast(backRay, out hit, rayLength, layer);
-      }
-      else
-      {
-        foundItem = true;
-      }
 
-      if (foundItem) {
-        Vector3 hitpos = hit.transform.position;
+      // Cast a ray backwards and forwards incase we are inside a collider
+      RaycastHit[] hits = Physics.RaycastAll(ray, rayLength, layer);
+      RaycastHit[] backHits = Physics.RaycastAll(backRay, rayLength, layer);
+
+      float closest = float.MaxValue;
+      GameObject hitObject = null;
+      // Iterate through all the interactables we found and find the closest
+      foreach (RaycastHit h in hits)
+      {
+        Vector3 hitpos = h.transform.position;
         hitpos.y = 0;
         Vector3 charpos = characterMesh.position;
         charpos.y = 0;
         float distance = (hitpos - charpos).magnitude;
-				if (distance <= grabDistance && Input.GetButtonDown ("A_P" + playerMovement.playerIndex)) {
-					GrabObject (hit.transform.gameObject);
-				}
-			}
+        if (distance <= grabDistance && distance < closest)
+        {
+          closest = distance;
+          hitObject = h.transform.gameObject;
+        }
+      }
+      foreach (RaycastHit h in backHits)
+      {
+        Vector3 hitpos = h.transform.position;
+        hitpos.y = 0;
+        Vector3 charpos = characterMesh.position;
+        charpos.y = 0;
+        float distance = (hitpos - charpos).magnitude;
+        if (distance <= grabDistance && distance < closest)
+        {
+          closest = distance;
+          hitObject = h.transform.gameObject;
+        }
+      }
 
-		}
-	}
+      // Grab the closest item that was inside the grab distance, if it exists
+      if (hitObject != null)
+      {
+        GrabObject(hitObject);
+      }
 
-	public void GrabObject (GameObject gb)
-	{
-		objectInHands = gb.transform.parent.gameObject;
-		objectInHands.transform.parent = characterMesh;
-		objectInHands.GetComponent<Rigidbody> ().isKinematic = true;
-		objectInHands.transform.position = characterMesh.position + new Vector3 (0, 2.3f, 0);
-		objectInHands.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
-		print ("Picking up object");
-	}
+    }
+  }
 
-	private void ReleaseObject ()
-	{
-		print ("Dropping " + objectInHands.transform.name);
-		Vector3 fwd = characterMesh.TransformDirection (Vector3.forward);
-		objectInHands.transform.parent = null;
-		objectInHands.GetComponent<Rigidbody> ().isKinematic = false;
-		objectInHands.GetComponent<Rigidbody> ().AddForce (fwd * 200 + new Vector3 (0, 200, 0));
-		objectInHands = null;
-	}
+  public void GrabObject(GameObject gb)
+  {
+    objectInHands = gb.transform.parent.gameObject;
+    objectInHands.transform.parent = characterMesh;
+    objectInHands.GetComponent<Rigidbody>().isKinematic = true;
+    objectInHands.transform.position = characterMesh.position + new Vector3(0, 2.3f, 0);
+    objectInHands.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+    print("Picking up object");
+  }
+
+  private void ReleaseObject()
+  {
+    print("Dropping " + objectInHands.transform.name);
+    Vector3 fwd = characterMesh.TransformDirection(Vector3.forward);
+    objectInHands.transform.parent = null;
+    objectInHands.GetComponent<Rigidbody>().isKinematic = false;
+    objectInHands.GetComponent<Rigidbody>().AddForce(fwd * 200 + new Vector3(0, 200, 0));
+    objectInHands = null;
+  }
 }
