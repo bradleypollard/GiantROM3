124545f87deede8be165553e8ee27e32993a6ad8
Simon Alenius <simon.s.alenius@gmail.com>
2016-08-13 13:39:17 +0200

    added a ring for the NPCs

---
+++ Assets/Scripts/rotate_innerRing.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate_innerRing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, -1, 0 * Time.deltaTime);
+    }
+}
