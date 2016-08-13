efed8bf0fdab57de319595ded613077c2c64ffbc
UaSirideain <man.dude.2@gmail.com>
2016-08-13 16:08:41 +0100

    Linked intercom to truck delivery

---
+++ Assets/Intercom.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Intercom : MonoBehaviour
+{
+	[Header("REFERENCES")]
+	[SerializeField] TruckDelivery deliveryScript;
+
+	List<int> playerID = new List<int>();
+
+	void Update()
+	{
+		foreach (int id in playerID)
+		{
+			if (Input.GetButton ("A_P" + id))
+			{
+				deliveryScript.HardDriveDelivery ();
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.GetComponent<PlayerMovement> ())
+		{
+			playerID.Add(collider.gameObject.GetComponent<PlayerMovement> ().playerIndex);
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		if (collider.GetComponent<PlayerMovement> ())
+		{	
+			playerID.Remove (collider.gameObject.GetComponent<PlayerMovement> ().playerIndex);
+		}
+	}
+}
