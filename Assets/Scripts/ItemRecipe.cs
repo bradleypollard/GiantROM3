5833a6f7975d338baa030d038e873d111e8f08a3
Adam Marsh <adam.marsh@rocketmail.com>
2016-08-14 00:45:14 +0100

    Item Recipe now Highlights next compatible item

--- Assets/Scripts/ItemRecipe.cs
+++ Assets/Scripts/ItemRecipe.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ItemRecipe : MonoBehaviour {
 
@@ -7,7 +8,48 @@ public class ItemRecipe : MonoBehaviour {
     public string IWorkWithThisMachine;
     public Renderer CopyColourFromHere;
 
-
     public Shader shaderOutline;
+
+    private List<Material> setFloatMats = new List<Material>();
+
+    void Update()
+    {
+        if (transform.root.name.Contains("Player"))
+        {
+            if (IWorkWithThisMachine == "Disk Burner")
+            {
+                GameObject.Find(IWorkWithThisMachine).transform.FindChild("computer/_computer").GetComponent<Renderer>().material.SetFloat("_OutlineTransparency", 1);
+                setFloatMats.Add(GameObject.Find(IWorkWithThisMachine).transform.FindChild("computer/_computer").GetComponent<Renderer>().material);
+            }
+
+            if (IWorkWithThisMachine == "Console")
+            {
+                foreach (GameObject console in GameObject.FindGameObjectsWithTag("Console"))
+                {
+                    console.transform.FindChild("mesh").GetComponent<Renderer>().material.SetFloat("_OutlineTransparency", 1);
+                    setFloatMats.Add(console.transform.FindChild("mesh").GetComponent<Renderer>().material);
+                }
+            }
+        }
+        else
+        {
+            RemoveOutline();
+        }
+    }
+
+    private void RemoveOutline()
+    {
+        foreach(Material m in setFloatMats)
+        {
+            m.SetFloat("_OutlineTransparency", 0);
+        }
+
+        setFloatMats.Clear();
+    }
+
+    void OnDestroy()
+    {
+        RemoveOutline();
+    }
 
 }
