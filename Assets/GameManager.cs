52e2417e560b075a2cd669f5c71265871d0af116
Adam Marsh <adam.marsh@rocketmail.com>
2016-08-14 12:07:27 +0100

    Time and Score working. Needs hooking into Title Sceen

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,17 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public int score;
 
-    private bool gameInProgress;
+    public bool gameInProgress;
     private float timer;
     public float gameLength;
     private float counter;
 
     public GameObject[] playerGbs;
+
+    public Text scoreText;
+    public Text timeText;
 
     void Update()
     {
@@ -26,23 +30,34 @@ public class GameManager : MonoBehaviour {
             {
                 GameFinished();
             }
+
+            scoreText.text = "Score: " + score;
+            timeText.text = "Time Left: " + (gameLength - counter);
         }
     }
 
     public void StartGame()
     {
-        foreach(GameObject gb in playerGbs)
-        {
-            gb.GetComponent<PlayerMovement>().enabled = true;
-        }
-
+        SetPlayerControllerState(true);
         gameInProgress = true;
+        scoreText.enabled = true;
+        timeText.enabled = true;
+    }
 
+    public void SetPlayerControllerState(bool state)
+    {
+        foreach (GameObject gb in playerGbs)
+        {
+            gb.GetComponent<PlayerMovement>().enabled = state;
+        }
     }
 
     public void GameFinished()
     {
-
+        SetPlayerControllerState(false);
+        gameInProgress = false;
+        scoreText.enabled = false;
+        timeText.enabled = false;
     }
 
     public void IncreaseScore(int points)
