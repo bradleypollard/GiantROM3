4c16e7172bd8cac9081c607c2e98127a95d96c9c
Adam Marsh <adam.marsh@rocketmail.com>
2016-08-14 11:28:23 +0100

    Inital Commit for GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public int score;
+
+    private bool gameInProgress;
+    private float timer;
+    public float gameLength;
+
+    public GameObject[] playerGBs;
+
+    void Update()
+    {
+        if (gameInProgress)
+        {
+            if (gameLength > 0)
+            {
+                gameLength -= Time.deltaTime;
+            }
+            else
+            {
+                GameFinished();
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        foreach(GameObject gb in playerGBs)
+        {
+            gb.GetComponent<PlayerMovement>().enabled = true;
+        }
+    }
+
+    public void GameFinished()
+    {
+
+    }
+
+    public void IncreaseScore(int points)
+    {
+        score += points;
+    }
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
